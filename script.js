c1876097d05dc6d018e3b5bfe3e3d38fd2ff8b27
Bianca Besciu <bianca.besciu@gmail.com>
2018-03-02 12:58:52 +0100

    implementing parseGuess

--- script.js
+++ script.js
@@ -62,8 +62,38 @@ const model = {
     },
 };
 
-
-
 // glue everything together including
 // by getting the player’s input and executing the game logic.
+const controller = {
+    guess: 0,
+
+    processGuess: function(guess) {
+        let location = parseGuess(guess);
+        if (location) {
+            
+        }
+    }
+};
+
+function parseGuess(guess) {
+    const alphabet = ["A", "B", "C", "D", "E", "F", "G"];
+
+    if (guess === null || guess.length !== 2) {
+        alert("Oops, please enter a letter and a number on the board.");
+    } else {
+        firstChar = guess.charAt(0);
+        let row = alphabet.indexOf(firstChar);
+        let column = guess.charAt(1);
+
+        if (isNaN(row) || isNaN(column)) {
+            alert("Oops, that isn't on the board.");
+        } else if (row < 0 || row >= model.boardSize || column < 0
+            || column >= model.boardSize) {
+            alert("Oops, that's off the board!");
+        } else {
+            return row + column;
+        }
+    }
+    return null;
+}
 
