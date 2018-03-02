6b542de4af75a31a455f56e383298a9f1bc704dc
Bianca Besciu <bianca.besciu@gmail.com>
2018-03-02 11:18:26 +0100

    implementing display message

---
+++ script.js
@@ -0,0 +1,14 @@
+let view = {
+    displayMessage: function(msg) {
+        let messageArea = document.getElementById('messageArea');
+        messageArea.innerHTML = msg;
+    },
+
+    displayHit: function(location) {
+
+    },
+
+    displayMiss: function(location) {
+
+    }
+};
