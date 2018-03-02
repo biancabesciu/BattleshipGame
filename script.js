e9b4fb5ec06f9c2b3cffcbe10b6c847dda11fa33
Bianca Besciu <bianca.besciu@gmail.com>
2018-03-02 11:58:10 +0100

    implementing the model

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// updates the display with hits, misses and messages for the user
 let view = {
     displayMessage: function(msg) {
         let messageArea = document.getElementById('messageArea');
@@ -15,3 +16,29 @@ let view = {
     }
 
 };
+
+// updates the state of the game
+//keeps track of ships - where they are, if they've been hit or sunk
+const model = {
+    bordSize: 7,
+    numShips: 3,
+    shipLenght: 3,
+    shipSunk: 0,
+
+    ships: [
+        {locations: ["10", "20", "30"], hits: ["", "", ""]},
+        {locations: ["32", "33", "34"], hits: ["", "", ""]},
+        {locations: ["63", "64", "65"], hits: ["", "", "hit"]}
+        ],
+
+    fire: function(guess) {
+        for(let i = 0; i < numShips.length; i++) {
+            let ship = this.ships[i];
+        }
+    }
+};
+
+
+
+// glue everything together including
+// by getting the player’s input and executing the game logic.
